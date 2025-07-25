16197c599fae160b0db0916c55367997c9f69c0f
Nargis-S <nargissathik1605@gmail.com>
2025-07-25 15:43:35 +0530

    Core Java 8

---
+++ C2TC_COREJAVA8/src/com/tnsif/dayone/FirstProgram.java
@@ -0,0 +1,9 @@
+
+//Demonstration of simple java program
+package com.tnsif.dayone;
+
+public class FirstProgram {
+		public static void main(String[] args) {
+			System.out.println("Hello World");
+		}
+	}
