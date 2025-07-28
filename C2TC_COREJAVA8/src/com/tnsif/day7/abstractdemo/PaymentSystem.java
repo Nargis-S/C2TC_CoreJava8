aa97a9dbe6ca3faf46971f0c5b7be8b1f76d98c6
Nargis-S <nargissathik1605@gmail.com>
2025-07-28 11:59:58 +0530

    Core Java 8

---
+++ C2TC_COREJAVA8/src/com/tnsif/day7/abstractdemo/PaymentSystem.java
@@ -0,0 +1,30 @@
+package com.tnsif.day7.abstractdemo;
+
+//abstract class
+
+abstract class PaymentGateway {
+	
+	public abstract void paymentProcess (double amt);
+	
+	public void connectToPayment() {
+		System.out.println("Connecting to the payment gateway..");
+	}
+
+}
+
+class CreditPayment extends PaymentGateway {
+	
+	@Override
+	public void paymentProcess (double amt) {
+		System.out.println("Processing the amount Rs." + amt);
+	}
+}
+
+public class PaymentSystem{
+	public static void main(String[] args) {
+		PaymentGateway payment;
+		payment = new CreditPayment();
+		payment.connectToPayment();
+		payment.paymentProcess(20000.0);
+	}
+}
